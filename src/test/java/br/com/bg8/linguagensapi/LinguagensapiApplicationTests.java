b7cbe8c56a7af6c9f720cb2c94c147c4feda6ae3
brunoguerra <brunoguerra@hotmail.com>
2023-04-07 16:19:09 -0300

    Initial commit

---
+++ src/test/java/br/com/bg8/linguagensapi/LinguagensapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bg8.linguagensapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguagensapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
