ce45619aa429ae370f9060a3449030a98bf201dc
brunoguerra <brunoguerra@hotmail.com>
2023-04-11 09:13:50 -0300

    fix:anconfigurações spring

--- src/main/java/br/com/bg8/linguagensapi/LinguagemController.java
+++ src/main/java/br/com/bg8/linguagensapi/LinguagemController.java
@@ -2,7 +2,6 @@ package br.com.bg8.linguagensapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
--- src/main/java/br/com/bg8/linguagensapi/LinguagemRepository.java
+++ src/main/java/br/com/bg8/linguagensapi/LinguagemRepository.java
@@ -1,9 +1,11 @@
 package br.com.bg8.linguagensapi;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LinguagemRepository extends MongoRepository<Linguagem, String> {
     List<Linguagem> findByOrderByRanking();
 }
